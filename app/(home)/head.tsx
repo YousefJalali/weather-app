29273097d3fa54ee77cac43e4591775ac4b374a5
Jbygoal <jbtgoal@gmail.com>
2023-07-07 02:13:16 +0300

    change app title

--- app/(home)/head.tsx
+++ app/(home)/head.tsx
@@ -1,12 +1,8 @@
 export default async function Head() {
   return (
-    <>
-      <title>Weather App</title>
-
-      <meta
-        name="viewport"
-        content="width=device-width, initial-scale=1, maximum-scale=1"
-      ></meta>
-    </>
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1, maximum-scale=1"
+    ></meta>
   )
 }
