73802c862cd8af3e9dd28728f332d06360a6aa02
Jbygoal <jbtgoal@gmail.com>
2023-07-07 02:15:48 +0300

    change app title

--- app/(home)/head.tsx
+++ app/(home)/head.tsx
@@ -1,8 +1,19 @@
 export default async function Head() {
   return (
-    <meta
-      name="viewport"
-      content="width=device-width, initial-scale=1, maximum-scale=1"
-    ></meta>
+    <>
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1, maximum-scale=1"
+      ></meta>
+      <title>WeatherNow</title>
+      <meta
+        name="description"
+        content="WeatherNow keeps you instantly updated on the current weather conditions and provides detailed forecasts for your location. Stay informed about temperature, precipitation, wind speed, and more at a glance."
+      />
+      <meta
+        name="keywords"
+        content="Real-time, Accurate, Instant, Up-to-date, Forecasts, Current, Temperature, Precipitation, Wind speed, Alerts, Interactive, User-friendly, Comprehensive, Hourly updates, Localized, Visuals, Radar, Severe weather, Notifications, Convenient"
+      />
+    </>
   )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,5 @@
 import '@/styles/globals.css'
 import { Poppins } from '@next/font/google'
-import Head from 'next/head'
-
 const font = Poppins({
   weight: ['100', '200', '400', '600', '700', '800'],
   subsets: ['latin'],
@@ -15,18 +13,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={font.variable}>
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>WeatherNow</title>
-        <meta
-          name="description"
-          content="WeatherNow keeps you instantly updated on the current weather conditions and provides detailed forecasts for your location. Stay informed about temperature, precipitation, wind speed, and more at a glance."
-        />
-        <meta
-          name="keywords"
-          content="Real-time, Accurate, Instant, Up-to-date, Forecasts, Current, Temperature, Precipitation, Wind speed, Alerts, Interactive, User-friendly, Comprehensive, Hourly updates, Localized, Visuals, Radar, Severe weather, Notifications, Convenient"
-        />
-      </Head>
       <body>
         <div className="font-xl md:font-2xl lg:font-3xl container mx-auto max-w-2xl bg-layout-level0 p-6 text-content-default">
           {children}
